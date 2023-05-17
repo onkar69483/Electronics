c14a8ef0e764efd63befdd53495b8a203723533f
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-05-17 22:06:53 +0530

    Update esptelegrambot.ino

--- esptelegrambot/esptelegrambot.ino
+++ esptelegrambot/esptelegrambot.ino
@@ -2,11 +2,11 @@
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 
-const char* ssid = "Galaxy";
-const char* password = "prem3562951413";
+const char* ssid = "";
+const char* password = "";
 
-#define BOTtoken "5651849401:AAEdhpyEUdbYx8Wx9pxgJvFJBHEIwQAq2JA"
-#define CHAT_ID "1093493235"
+#define BOTtoken ""
+#define CHAT_ID ""
 
 int button = 5;
 int led = 4;
