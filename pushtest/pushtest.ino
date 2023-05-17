a20bdf1b91c484763b89a4386545f724ff650186
Onkar Mendhapurkar <61963755+onkar69483@users.noreply.github.com>
2023-05-17 22:11:49 +0530

    Update pushtest.ino

--- pushtest/pushtest.ino
+++ pushtest/pushtest.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "Galaxy";
-const char* password = "12345678";
+const char* ssid = "";
+const char* password = "";
 
 ESP8266WebServer server(80);
 
